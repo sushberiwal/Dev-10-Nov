f331a2e88981893d547200965f4840530db98029
sushberiwal <sushantberiwal@gmail.com>
2020-11-11 12:20:02 +0530

    cheerio intro

---
+++ Lec2_WebScrapping/facts/readFile.js
@@ -0,0 +1,37 @@
+// file system 
+// fs module => file system 
+// import 
+
+// file system is imported from nodejs to node application (readFile.js);
+let fs = require("fs");
+let cheerio = require("cheerio");
+
+
+// let f1KaData =  fs.readFileSync("./f1.txt" , "utf-8");
+// console.log(f1KaData);
+
+
+// html selectors
+let htmlKaData = fs.readFileSync("./index.html" , "utf-8");
+
+
+let ch = cheerio.load(htmlKaData);
+
+// h1 ka data
+
+// <h1>Heading 1</h1>
+let h1KaData = ch("#main").text();
+console.log(h1KaData);
+
+// p tag ka data
+let pKaData = ch("ul .outer").text();
+console.log(pKaData);
+
+// let pKaData = ch(".outer.unique").text();
+// console.log(pKaData);
+
+// <h1>Heading 1</h1>
+let h1Element = ch("h1");
+console.log(h1Element);
+
+
