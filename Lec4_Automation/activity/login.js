e4ba0af9854b7d051f578c9ba474dc5a3ecf42ea
sushberiwal <sushantberiwal@gmail.com>
2020-11-17 18:11:01 +0530

    one ques solved

--- Lec4_Automation/activity/login.js
+++ Lec4_Automation/activity/login.js
@@ -5,7 +5,8 @@ const id = "mapovif924@testbnk.com";
 const pw = "12345678";
 
 let tab;
-
+let idx;
+let gCode;
 // puppeteer functions => pending promise
 
 // initialize a new browser window
@@ -34,7 +35,7 @@ windowOpenPromise
     return idTypedPromise;
   })
   .then(function () {
-    let pwTypedPromise = tab.type("#input-2", pw);
+    let pwTypedPromise = tab.type("#input-2", pw); 
     return pwTypedPromise;
   })
   .then(function () {
@@ -59,14 +60,17 @@ windowOpenPromise
     return waitPromise;
   })
   .then(function () {
+    // document.querySelectorAll(".js-track-click.challenge-list-item");
     let allATagsPromise = tab.$$(".js-track-click.challenge-list-item");
-    return allATagsPromise;
+    return allATagsPromise; //Promise<Pending>
   })
-  .then(function (allATags) {
-    // [ <a> </a>   ,  <a> </a>   , <a> </a>   , <a> </a>   ];
+  .then(function(allATags) {
+    // [ <a href=""> </a>   ,  <a> </a>   , <a> </a>   , <a> </a>   ];
     // let allLinksPromise = [Promise<data> , Promise<data> ,Promise<data> ,Promise<data> ];
     let allLinksPromise = [];
+    
     for (let i = 0; i < allATags.length; i++) {
+      // tab.evaluate(cb function  , element   ) and return a pending promise
       let linkPromise = tab.evaluate(function (elem) {
         return elem.getAttribute("href");
       }, allATags[i]);
@@ -82,7 +86,12 @@ windowOpenPromise
     let completeLinks = allLinks.map( function(link){
         return "https://www.hackerrank.com"+link;
     })
-    console.log(completeLinks);
+    // console.log(completeLinks);
+    let quesSolvedPromise = solveQuestion(completeLinks[0]);
+    return quesSolvedPromise;
+  })
+  .then(function(){
+    console.log("One ques solved");
   })
   .catch(function(error){
       console.log(error);
@@ -106,4 +115,129 @@ windowOpenPromise
 
 
 
+  }
+
+
+  function getCode(){
+    return new Promise(function(resolve , reject){
+      let waitPromise = tab.waitForSelector(".hackdown-content h3" , {visible:true});
+      waitPromise.then(function(){
+        let codeNamesTagsPromise = tab.$$(".hackdown-content h3");
+        return codeNamesTagsPromise;
+      })
+      .then(function(codeNamesTags){
+        // [ <h3>C++</h3> , <h3>Python</h3> , <h3>Java</h3>    ]
+        let allCodeNamesPromise = [];
+        for(let i=0 ; i<codeNamesTags.length ; i++){
+        let codeNamePromise =   tab.evaluate( function(elem){   return elem.textContent;     }   ,  codeNamesTags[i]);
+        allCodeNamesPromise.push(codeNamePromise);
+        }
+        let pendingPromise = Promise.all(allCodeNamesPromise);
+        return pendingPromise;
+      })
+      .then(function(allCodeNames){
+        // [ "C++" , "Python" , "Java"  ];
+        idx;
+        for(let i=0 ; i<allCodeNames.length ; i++){
+          if(allCodeNames[i] == "C++"){
+            idx = i;
+            break;
+          }
+        }
+        let allCodeElementsPromise = tab.$$(".hackdown-content .highlight");
+        return allCodeElementsPromise;
+      })
+      .then(function(allCodeElements){
+        // [  <div class="highlight"> </div>  , <div class="highlight"> </div>  , <div class="highlight"> </div>  ];
+        let codeDiv = allCodeElements[idx];
+        let codePromise = tab.evaluate( function(elem){ return elem.textContent;  }   , codeDiv)
+        return codePromise;
+      })
+      .then(function(code){
+        // console.log(code);
+        gCode = code;
+        resolve();
+      })
+      .catch(function(error){
+        reject(error);
+      })
+    })
+  }
+
+
+  function pasteCode(){
+    return new Promise(function(resolve ,reject){
+      let waitAndClickPromise = waitAndClick(".custom-input-checkbox");
+      waitAndClickPromise.then(function(){
+        let codeTypedPromise = tab.type(".custominput" , gCode);
+        return codeTypedPromise;
+      })
+      .then(function(){
+        let ctrlKeyHoldPromise = tab.keyboard.down('Control');
+        return ctrlKeyHoldPromise;
+      })
+      .then(function(){
+        let aKeyPressedPromise = tab.keyboard.press('A');
+        return aKeyPressedPromise;
+      })
+      .then(function(){
+        let xKeyPressedPromise = tab.keyboard.press('X');
+        return xKeyPressedPromise;
+      })
+      .then(function(){
+        let codeBoxClickedPromise = tab.click(".monaco-scrollable-element.editor-scrollable.vs");
+        return codeBoxClickedPromise;
+      })
+      .then(function(){
+        let aKeyPressedPromise = tab.keyboard.press('A');
+        return aKeyPressedPromise;
+      })
+      .then(function(){
+        let aKeyPressedPromise = tab.keyboard.press('V');
+        return aKeyPressedPromise;
+      })
+      .then(function(){
+        let ctrlKeyUpPromise = tab.keyboard.up('Control');
+        return ctrlKeyUpPromise;
+      })
+      .then(function(){
+        resolve();
+      })
+      .catch(function(error){
+        reject(error);
+      })
+
+
+    })
+  }
+  function solveQuestion(qLink){
+    return new Promise( function(resolve , reject){
+      let questionOpenedPromise = tab.goto(qLink);
+      questionOpenedPromise.then(function(){
+        let waitAndClickPromise = waitAndClick("#Editorial");
+        return waitAndClickPromise;
+      })
+      .then(function(){
+        let getCodePromise = getCode();
+        return getCodePromise;
+      })
+      .then(function(){
+        let problemTabClickedPromise = tab.click("#Problem");
+        return problemTabClickedPromise;
+      })
+      .then(function(){
+        let codePastedPromise = pasteCode();
+        return codePastedPromise;
+      })
+      .then(function(){
+        let submitCodePromise = tab.click(".pull-right.btn.btn-primary.hr-monaco-submit");
+        return submitCodePromise;
+      })
+      .then(function(){
+        resolve();
+      })
+      .catch(function(error){
+        reject(error);
+      })
+    })
   }
