32e65471cb21e8f2d8826daacabd23087e722130
sushberiwal <sushantberiwal@gmail.com>
2020-11-24 22:16:00 +0530

    excel final

--- Lec7_Electron_Excel/activity/script.js
+++ Lec7_Electron_Excel/activity/script.js
@@ -3,10 +3,98 @@ const $ = require("jquery");
 const dialog = require("electron").remote.dialog;
 const fs = require("fs");
 
-
 $(document).ready(function () {
-  let db;
+  let sheetsDb = [];
+
+  let db; // current db
   let lsc; // last selected cell
+
+  // sheets add
+  $(".sheets-add").on("click", function () {
+    // remove active sheet
+    $(".sheet.active-sheet").removeClass("active-sheet");
+    // make a new sheet
+    let sheet = `<div class="sheet active-sheet" sid=${sheetsDb.length}>Sheet ${
+      sheetsDb.length + 1
+    }</div>`;
+    // append div to sheets content !!
+    $(".sheets-content").append(sheet);
+
+    $(".active-sheet").on("click", function () {
+      if (!$(this).hasClass("active-sheet")) {
+        let sheetId = $(this).attr("sid");
+        $(".sheet.active-sheet").removeClass("active-sheet");
+        $(this).addClass("active-sheet");
+        db = sheetsDb[sheetId];
+        // ui set
+        for (let i = 0; i < 100; i++) {
+          for (let j = 0; j < 26; j++) {
+            let cellObject = db[i][j];
+            $(`.cell[rowid=${i}][colid=${j}]`).html(cellObject.value);
+          }
+        }
+      }
+    });
+
+    init(); // db new and push in sheetsDb;
+
+    // ui new
+    for (let i = 0; i < 100; i++) {
+      for (let j = 0; j < 26; j++) {
+        $(`.cell[rowid=${i}][colid=${j}]`).html("");
+      }
+    }
+  });
+
+  $(".sheet").on("click", function () {
+    if (!$(this).hasClass("active-sheet")) {
+      let sheetId = $(this).attr("sid");
+      $(".sheet.active-sheet").removeClass("active-sheet");
+      $(this).addClass("active-sheet");
+      db = sheetsDb[sheetId];
+      // ui set
+      for (let i = 0; i < 100; i++) {
+        for (let j = 0; j < 26; j++) {
+          let cellObject = db[i][j];
+          $(`.cell[rowid=${i}][colid=${j}]`).html(cellObject.value);
+        }
+      }
+    }
+  });
+
+
+
+  //bold // underline // italic
+  $(".font-styling button").on("click" , function(){
+    let id = $(this).attr("id");
+    let rowId = $(lsc).attr("rowid");
+    let colId = $(lsc).attr("colid");
+    let cellObject = db[rowId][colId];
+    if(id == "bold"){
+      $(lsc).css("font-weight" , cellObject.fontStyle.bold ? "normal" : "bold");
+      cellObject.fontStyle.bold = !cellObject.fontStyle.bold;
+    }
+    else if(id=="underline"){
+      $(lsc).css("text-decoration" , cellObject.fontStyle.underline ? "none" : "underline");
+      cellObject.fontStyle.underline = !cellObject.fontStyle.underline;
+    }
+    else{
+      $(lsc).css("font-style" , cellObject.fontStyle.italic ? "normal" : "italic");
+      cellObject.fontStyle.italic = !cellObject.fontStyle.italic;
+    }
+  })
+
+
+
+  // text -align
+  $(".menu3 button").on("click" , function(){
+    let id = $(this).attr("id");
+    $(lsc).css("text-align" , id);
+    let rowId = $(lsc).attr("rowid");
+    let colId = $(lsc).attr("colid");
+    let cellObject = db[rowId][colId];
+    cellObject.textAlign = id;
+  })
 
   $(".cell").on("click", function () {
     console.log(this);
@@ -20,104 +108,108 @@ $(document).ready(function () {
   });
 
 
+  // font-size
+  $("#font-size").on("change" , function(){
+    let fontSize = $(this).val();
+    $(lsc).css("font-size" , fontSize+"px");
+    let rowId = $(lsc).attr("rowid");
+    let colId = $(lsc).attr("colid");
+    let cellObject = db[rowId][colId];
+    cellObject.fontSize = fontSize+"px";
+  })
 
-  $(".file").on("click" , function(){
+  $(".file").on("click", function () {
     $(this).addClass("active-menu");
     $(".home").removeClass("active-menu");
     // filemenuOptions se hide remove
     $(".file-menu-options").removeClass("hide");
     // hide lag jae home-menu-options
     $(".home-menu-options").addClass("hide");
-  })
+  });
 
-
-  $(".home").on("click" , function(){
+  $(".home").on("click", function () {
     $(this).addClass("active-menu");
     $(".file").removeClass("active-menu");
-     // filemenuOptions pe hide 
-     $(".file-menu-options").addClass("hide");
+    // filemenuOptions pe hide
+    $(".file-menu-options").addClass("hide");
     // hide remove  home-menu-options
     $(".home-menu-options").removeClass("hide");
-  })
-
-
+  });
 
   // new file
-
-  $(".new").on("click" , function(){
-    db=[];
-    for(let i=0 ; i<100 ; i++){
+  $(".new").on("click", function () {
+    db = [];
+    $("#address").val('');
+    $("#formula").val('');
+    for (let i = 0; i < 100; i++) {
       let row = [];
-      for(let j=0 ; j<26 ; j++){
-        $(`.cell[rowid=${i}][colid=${j}]`).html('');
+      for (let j = 0; j < 26; j++) {
+        $(`.cell[rowid=${i}][colid=${j}]`).html("");
         let name = String.fromCharCode(65 + j) + (i + 1);
         let cellObject = {
           name: name,
           value: "",
           formula: "",
           childrens: [],
-          parents: []
+          parents: [],
         };
         row.push(cellObject);
       }
       db.push(row);
     }
-  })
+  });
 
   // open file
-  $(".open").on("click" , function(){
+  $(".open").on("click", function () {
     let files = dialog.showOpenDialogSync();
     let data = fs.readFileSync(files[0]);
     db = JSON.parse(data);
     // database updated
     // ui ???
-    let count=0;
-    for(let i=0 ; i<100 ; i++){
-      for(let j=0 ; j<26 ; j++){
+    let count = 0;
+    for (let i = 0; i < 100; i++) {
+      for (let j = 0; j < 26; j++) {
         let cellObject = db[i][j];
         console.log(count);
         count++;
         $(`.cell[rowid=${i}][colid=${j}]`).text(cellObject.value);
       }
     }
-  })
+  });
 
   // save file
-  $(".save").on("click" , function(){
+  $(".save").on("click", function () {
     let filePath = dialog.showSaveDialogSync();
-    if(filePath){
-      fs.writeFileSync(filePath , JSON.stringify(db));
-      alert("FILE SAVED SUCCESFULLY !!!")
-    }else{
+    if (filePath) {
+      fs.writeFileSync(filePath, JSON.stringify(db));
+      alert("FILE SAVED SUCCESFULLY !!!");
+    } else {
       alert("FILE NOT SAVED");
     }
-  })
+  });
 
-
-  $(".cell").on("keypress , keydown" , function(){
+  $(".cell").on("keypress , keydown", function () {
     console.log("key pressed !!!");
     let cellId = $(this).attr("rowid");
     let newHeight = $(this).height();
     $(`.left-col-cell[cellid=${cellId}]`).height(newHeight);
-  })
-
+  });
 
   // scrolling
-  $(".content").on("scroll" , function(){
+  $(".content").on("scroll", function () {
     let topOffSet = $(this).scrollTop();
-    let leftOffSet =  $(this).scrollLeft();
+    let leftOffSet = $(this).scrollLeft();
 
-    $(".top-left-col").css("top" , topOffSet+"px");
-    $(".top-left-col").css("left" , leftOffSet+"px");
+    $(".top-left-col").css("top", topOffSet + "px");
+    $(".top-left-col").css("left", leftOffSet + "px");
 
-    $(".top-row").css("top" , topOffSet+"px");
-    $(".left-col").css("left"  ,leftOffSet+"px");
-  })
+    $(".top-row").css("top", topOffSet + "px");
+    $(".left-col").css("left", leftOffSet + "px");
+  });
 
-
-  $(".cell").on("keypress" , function(){
-      console.log("key pressed !!");
-  })
+  $(".cell").on("keypress", function () {
+    console.log("key pressed !!");
+  });
 
   $(".cell").on("blur", function () {
     lsc = this;
@@ -125,7 +217,7 @@ $(document).ready(function () {
     let colId = Number($(this).attr("colid"));
     let cellObject = db[rowId][colId];
     let value = $(this).html();
-    if( value != cellObject.value ) {
+    if (value != cellObject.value) {
       cellObject.value = value;
       if (cellObject.formula) {
         removeFormula(cellObject);
@@ -247,14 +339,20 @@ $(document).ready(function () {
           value: "",
           formula: "",
           childrens: [],
-          parents: []
+          parents: [],
+          fontStyle : { bold : false , underline : false , italic : false },
+          textAlign : "left",
+          fontSize : "16px",
+          color : {cellColor : "white" , fontColor:"black"}
         };
         row.push(cellObject);
       }
       db.push(row);
     }
     // db initialize
-    console.log(db);
+    sheetsDb.push(db);
+    // console.log(db);
+    console.log(sheetsDb);
   }
   init();
 });
