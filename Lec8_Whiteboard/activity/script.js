24e17e1b9d540555743c33f11da33a494f38fe32
Sushant <sushantberiwal@gmail.com>
2021-01-03 19:38:07 +0530

    drawing logic

--- Lec8_Whiteboard/activity/script.js
+++ Lec8_Whiteboard/activity/script.js
@@ -13,11 +13,37 @@ window.addEventListener("resize" , function(){
 })
 
 
+// ctx.beginPath();
+// ctx.moveTo(20,50);
+// ctx.lineTo(100,150);
+// ctx.stroke();
 
 
-ctx.beginPath();
-ctx.moveTo(20,50);
-ctx.lineTo(100,150);
-ctx.stroke();
+let isPenDown = false;
+
+canvas.addEventListener("mousedown" , function(e){
+    isPenDown = true;
+    let x = e.clientX;
+    let y = e.clientY-canvasTop;
+    // console.log(x,y);
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+})
+
+canvas.addEventListener("mousemove" , function(e){
+    if(isPenDown==true){
+        let x = e.clientX;
+        let y = e.clientY-canvasTop;
+        // console.log(x,y);
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
+})
+
+
+canvas.addEventListener("mouseup" , function(e){
+    isPenDown = false;
+})
+
 
 
